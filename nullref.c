6af8b91599615b9625c47f769b98a95b00ecb968
Jon Baird <jbaird29@gmail.com>
2022-04-28 09:30:43 -0700

    fix test program

--- nullref.c
+++ nullref.c
@@ -3,7 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-  int *nullptr = 0;  // create a NULL pointer
-  printf(1, "%d", *nullptr);  // dereference and see what prints
+  void *nullptr = ((void*) 0);  // create a NULL pointer
+  int i = *(int *)nullptr;
+  i++;
+  // printf(1, "%d", *(int *)nullptr);  // dereference and see what prints
   exit();
 }
