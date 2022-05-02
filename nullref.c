44286664c71b0112279de791e290bb41d08cd0e5
Jon Baird <jbaird29@gmail.com>
2022-05-02 12:08:59 -0700

    update nullref test to accept argument

--- nullref.c
+++ nullref.c
@@ -3,10 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-  int *nullptr = 0;  // create a NULL pointer
-  for(int i = 0; i < (0x1000 / sizeof(int)); i++) {
-    printf(1, "%d, ", *nullptr);  // dereference and see what prints
-    nullptr++;
+  if(argc < 2) {
+    printf(1, "Enter 'nullref {d}' where {d} is the decimal address to print");
+    exit();
   }
+  int addr = atoi(argv[1]);  // get the argument
+  printf(1, "%x\n", *(int *)addr);  // print value at that address
   exit();
 }
